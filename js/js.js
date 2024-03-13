857c1bfa3725784e753da9a2d1d1224163978c37
Coral <yashan.tsai@gmail.com>
2024-03-13 12:00:42 +0800

    backToTop & Fab

--- js/js.js
+++ js/js.js
@@ -18,3 +18,24 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   });
 });
+
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
