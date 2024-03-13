ba37256db4009c35043705d4857c7073d8f0e56d
Coral <yashan.tsai@gmail.com>
2024-03-13 22:07:56 +0800

    typing & protrait

--- js/js.js
+++ js/js.js
@@ -19,23 +19,18 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 });
 
-let mybutton = document.getElementById("myBtn");
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function () {
-  scrollFunction();
-};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
+function protectpasscode() {
+  const result = document.getElementById("tutorial").value;
+  let passcode = 12345;
+  let space = "";
+  if (result == space) {
+    alert("Type passcode");
   } else {
-    mybutton.style.display = "none";
+    if (result == passcode) {
+      document.write("<center><h1>TP, The Best E-Learning </h1></center>");
+    } else {
+      alert("Incorrect Passcode");
+      location.reload();
+    }
   }
-}
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
 }
